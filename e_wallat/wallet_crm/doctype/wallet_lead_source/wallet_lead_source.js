ae762e21dc5ae66744da6041319bd24235aecdcc
atrab-ahmed <aldubai.atrab@gmail.com>
2024-09-02 02:45:56 +0300

    feat(Wallet_CRM): Wallet Lead and Wallet Lead Source DocTypes with their fields

---
+++ e_wallat/wallet_crm/doctype/wallet_lead_source/wallet_lead_source.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Digital Fintech Box and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Wallet Lead Source", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ e_wallat/wallet_crm/doctype/wallet_lead_source/wallet_lead_source.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Digital Fintech Box and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WalletLeadSource(Document):
+	pass
